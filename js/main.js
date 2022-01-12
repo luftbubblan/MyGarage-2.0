4bbb8b926011f40f29dabe4ef1cb9290f6c6228f
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-01-12 09:37:17 +0100

    park and show and exit works, and menu works

--- js/main.js
+++ js/main.js
@@ -8,24 +8,63 @@ class Car {
     }
     
     present() {
-        return `The car in parkingspace ${this.parkingSpace} are a ${this.color} ${this.brand} ${this.model} with platenumber ${this.plateNumber}`;
+        return `Parkingspace:${this.parkingSpace}\nBrand:${this.brand}\nModel:${this.model}\nColor:${this.color}\nPlatenumber:${this.plateNumber}`;
     }
 }
 
 let garage = {
     numberOfCars: 0,
+    parkingSpace: [],
+
+    parkCar: function(newCar) {
+        this.parkingSpace.push(newCar);
+        this.numberOfCars++;
+        return newCar;
+    },
 }
 
-let parkingSpace = [];
 
-let brand = prompt("Brand")
-let model = prompt("Model")
-let platenumber = prompt("Platenumber")
-let color = prompt("Color")
-let position = prompt("Parkingspace position")
-
-parkingSpace.push(new Car(brand, model, platenumber, color, position));
-
-for(let i in parkingSpace) {
-    alert(parkingSpace[i].present())
+parkCar = () => {
+    let brand = prompt("PARK A CAR\nBrand")
+    let model = prompt("PARK A CAR\nModel")
+    let platenumber = prompt("PARK A CAR\nPlatenumber")
+    let color = prompt("PARK A CAR\nColor")
+    let position = prompt("PARK A CAR\nParkingspace position")
+    
+    garage.parkCar(new Car(brand, model, platenumber, color, position));
 }
+
+
+showParkedCars = () => {
+    for(let i in garage.parkingSpace) {
+        alert(`SHOW PARKED CARS\n${garage.parkingSpace[i].present()}`)
+    }
+}
+
+
+menu = () => {
+    while(runMenu) {
+        menuChoice = prompt('MENU\ntype "park" to park a new car\ntype "show" to show all parked cars\ntype "exit" to exit');
+        switch(menuChoice) {
+            case "park":
+                console.log("park a car");
+                parkCar();
+                break;
+            case "show":
+                console.log("show all cars");
+                showParkedCars();
+                break;
+            case "exit":
+                console.log("exit");
+                runMenu = false;
+                break;
+            default:
+                console.log("Menu default")
+                break;
+        }
+    }
+}
+
+let runMenu = true;
+let menuChoice = "";
+menu();
