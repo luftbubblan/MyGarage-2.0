61db99f8b78b0b3b4a3b8d339443835b620455ba
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-01-14 13:17:22 +0100

    sorting works and parking check works

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ class Car {
     }
     
     present() {
-        return `Parkingspace:${this.parkingSpace}\nBrand:${this.brand}\nModel:${this.model}\nColor:${this.color}\nPlatenumber:${this.plateNumber}`;
+        return `Parkingspace: ${this.parkingSpace}\nBrand: ${this.brand}\nModel: ${this.model}\nColor: ${this.color}\nPlatenumber: ${this.plateNumber}`;
     }
 }
+
 
 let garage = {
     numberOfCars: 0,
@@ -27,11 +28,47 @@ let garage = {
 parkCar = () => {
     let brand = prompt("PARK A CAR\nBrand")
     let model = prompt("PARK A CAR\nModel")
-    let platenumber = prompt("PARK A CAR\nPlatenumber")
+    let plateNumber = prompt("PARK A CAR\nPlatenumber")
     let color = prompt("PARK A CAR\nColor")
-    let position = prompt("PARK A CAR\nParkingspace position")
-    
-    garage.parkCar(new Car(brand, model, platenumber, color, position));
+    let parkingSpace = prompt("PARK A CAR\nParkingspace position")
+
+    //checks so the position is valid
+    parkingSpace = positionCheck(parkingSpace);
+
+    //adds the car to the garage
+    garage.parkCar(new Car(brand, model, plateNumber, color, parkingSpace));
+
+    //sorts the garage
+    sortingTheGarageAfterParkingspacePosition(garage.parkingSpace);
+}
+
+
+sortingTheGarageAfterParkingspacePosition = (allParkedCars) => {
+    // console.log("sorting");
+    allParkedCars.sort(function(a, b){return a.parkingSpace - b.parkingSpace});
+}
+
+
+positionCheck = (input) => {
+    //checks if the position is a positive number
+    if(Math.sign(input) != 1) {
+        input = prompt('PARKING SPACE NEEDS TO BE A POSITIVE NUMBER\nWhat parking space do you want to park the car in (positive number)');
+        input = positionCheck(input);
+    }
+
+    //checks if the space is occupied and informs the user what spaces are occupied
+    let freeSpaces = "";
+    for(let i in garage.parkingSpace) {
+        freeSpaces += garage.parkingSpace[i].parkingSpace + ", ";
+    }
+
+    for(let i in garage.parkingSpace) {
+        if(input == garage.parkingSpace[i].parkingSpace) {
+            input = prompt(`PARKING SPACE IS ALREADY OCCUPIED\n${freeSpaces} are all occupied\nWhat parking space do you want to park the car in (positive number)`);
+            input = positionCheck(input);
+        }
+    }
+    return input;
 }
 
 
