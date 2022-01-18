de5d1f9b4aa3007566ee782caa20fe4c75cb4882
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-01-18 17:01:36 +0100

    show car done

--- js/main.js
+++ js/main.js
@@ -1,65 +1,77 @@
 class Car {
-    constructor (brand, model, plateNumber, color, parkingSpace) {
+    constructor (brand, model, plateNumber, color, parkingspot) {
         this.brand = brand;
         this.model = model;
         this.plateNumber = plateNumber;
         this.color = color;
-        this.parkingSpace = parkingSpace;
+        this.parkingspot = parkingspot;
     }
     
 
     present() {
-        return `Parkingspace: ${this.parkingSpace}<br>Brand: ${this.brand}<br>Model: ${this.model}<br>Color: ${this.color}<br>Platenumber: ${this.plateNumber}<br><br>`;
+        return `Parkingspot: ${this.parkingspot}<br>Brand: ${this.brand}<br>Model: ${this.model}<br>Color: ${this.color}<br>Platenumber: ${this.plateNumber}<br><br>`;
     }
 }
 
 
 let garage = {
     numberOfCars: 0,
-    parkingSpaces: [],
+    parkingspots: [],
     
 
 
     removeCar: function(positionInArray) {
         console.log("Method: removeCar");
-        this.parkingSpaces.splice(positionInArray, 1);
+        this.parkingspots.splice(positionInArray, 1);
         this.numberOfCars--;
     },
 
     sortGarage: function() {
         console.log("Method: sortingTheGarage")
-        this.parkingSpaces.sort(function(a, b){return a.parkingSpace - b.parkingSpace});
+        this.parkingspots.sort(function(a, b){return a.parkingspot - b.parkingspot});
     },
 
     unparkCar: function() {
         console.log("Method: unparkCar");
-        parkingSpace = prompt("UNPARK A CAR\nremove the car in parkingspace");
-        for(let i in this.parkingSpaces) {
-            if(parkingSpace == this.parkingSpaces[i].parkingSpace) {
+        parkingspot = prompt("UNPARK A CAR\nRemove the car in parkingspot");
+        for(let i in this.parkingspots) {
+            if(parkingspot == this.parkingspots[i].parkingspot) {
                 this.removeCar(i);
             }
         }
-        // change the parkingspace to grey in the grid
-        this.changeToGrey(parkingSpace);
+        // change the parkingspot to grey in the grid
+        this.changeToGrey(parkingspot);
     },
 
-    showCarInSpace: function() {
-        console.log("Method: showCarInSpace");
-        parkingSpace = prompt("SHOW CAR\nshow the car in parkingspace");
+    showCarInSpot: function() {
+        console.log("Method: showCarInspot");
+        parkingspot = prompt("SHOW CAR\nShow the car in parkingspot");
 
-        if(parkingSpace > 14) {
-            parkingSpace = prompt("SHOW CAR\nthe garage only has 14 parkingspaces\nshow the car in parkingspace");
+        parkingspot = this.spotCheck(parkingspot)
+
+        
+    },
+
+    spotCheck: function(parkingspot) {
+        console.log("Method: spotCheck");
+        //checks if the position is a positive number and 1-14
+        if(Math.sign(parkingspot) != 1  || parkingspot > 14 || parkingspot < 1) {
+            parkingspot = prompt('SHOW CAR\nThe parkingspot need to be a positive number\nWhat car do you want to show (1-14)');
+            parkingspot = this.spotCheck(parkingspot);
         }
-        spaceIsEmpty = true;
-        for(let i in this.parkingSpaces) {
-            if(parkingSpace == this.parkingSpaces[i].parkingSpace) {
-                document.getElementById("output").innerHTML = this.parkingSpaces[i].present();
-                spaceIsEmpty = false;
+
+        // checks if the spot is empty or not
+        spotIsEmpty = true;
+        for(let i in this.parkingspots) {
+            if(parkingspot == this.parkingspots[i].parkingspot) {
+                document.getElementById("output").innerHTML = this.parkingspots[i].present();
+                spotIsEmpty = false;
             }
         }
-        if(spaceIsEmpty == true) {
-            document.getElementById("output").innerHTML = "There is no car parked in parkingspace " + parkingSpace;
+        if(spotIsEmpty == true) {
+            document.getElementById("output").innerHTML = "There is no car parked in parkingspot " + parkingspot;
         }
+        return parkingspot
     },
 
     parkCar: function() {
@@ -80,55 +92,50 @@ let garage = {
         while(color === "") {
             color = prompt("PARK A CAR\nWhat Color");
         }
-        parkingSpace = prompt("PARK A CAR\nPick a free parkingspace");
+        parkingspot = prompt("PARK A CAR\nPick a free parkingspot");
     
         //checks so the position is valid
-        parkingSpace = this.positionCheck(parkingSpace);
+        parkingspot = this.parkingCheck(parkingspot);
 
-        // change the parkingspace to red in the grid
-        this.changeToRed(parkingSpace);
+        // change the parkingspot to red in the grid
+        this.changeToRed(parkingspot);
     
         //adds the car to the garage
-        this.parkingSpaces.push(new Car(brand, model, plateNumber, color, parkingSpace));
+        this.parkingspots.push(new Car(brand, model, plateNumber, color, parkingspot));
         this.numberOfCars++;
     },
 
-    positionCheck: function(parkingSpace) {
-        console.log("Method: positionCheck");
-        //checks if the position is a positive number
-        if(Math.sign(parkingSpace) != 1) {
-            parkingSpace = prompt('PARKING SPACE NEEDS TO BE A POSITIVE NUMBER\nWhat parking space do you want to park the car in (1-14)');
-            parkingSpace = this.positionCheck(parkingSpace);
-        }
-
-        if(parkingSpace > 14) {
-            parkingSpace = prompt('THE GARAGE ONLY HAS 14 PARKINGSPACES\nParkingspace position 1-14');
-            parkingSpace = this.positionCheck(parkingSpace);
+    parkingCheck: function(parkingspot) {
+        console.log("Method: parkingCheck");
+        //checks if the position is a positive number and 1-14
+        if(Math.sign(parkingspot) != 1 || parkingspot > 14) {
+            parkingspot = prompt('PARKINGSPOT NEED TO BE A POSITIVE NUMBER\nWhat parkingspot do you want to park the car in (1-14)');
+            parkingspot = this.parkingCheck(parkingspot);
         }
     
-        //checks if the space is occupied and informs the user what spaces are occupied
-        freeSpaces = ""
-        for(let i in this.parkingSpaces) {
-            freeSpaces += this.parkingSpaces[i].parkingSpace + ", ";
+        //checks if the spot is occupied and informs the user what spots are occupied
+        freespots = ""
+        for(let i in this.parkingspots) {
+            freespots += this.parkingspots[i].parkingspot + ", ";
         }
         
-        for(let i in this.parkingSpaces) {
-            if(parkingSpace == this.parkingSpaces[i].parkingSpace) {
-                parkingSpace = prompt(`PARKING SPACE IS ALREADY OCCUPIED\n${freeSpaces} are all occupied\nWhat parking space do you want to park the car in (1-14)`);
-                parkingSpace = this.positionCheck(parkingSpace);
+        for(let i in this.parkingspots) {
+            if(parkingspot == this.parkingspots[i].parkingspot) {
+                parkingspot = prompt(`PARKINGSPOT IS ALREADY OCCUPIED\n${freespots} are all occupied\nWhat parking spot do you want to park the car in (1-14)`);
+                parkingspot = this.parkingCheck(parkingspot);
             }
         }
-        return parkingSpace;
+        return parkingspot;
     },
 
-    changeToRed: function(parkingSpace) {
+    changeToRed: function(parkingspot) {
         console.log("Method: changeToRed");
-        document.getElementById("pos" + parkingSpace).style.backgroundColor = "red";
+        document.getElementById("pos" + parkingspot).style.backgroundColor = "red";
     },
 
-    changeToGrey: function(parkingSpace) {
+    changeToGrey: function(parkingspot) {
         console.log("Method: changeToGray");
-        document.getElementById("pos" + parkingSpace).style.backgroundColor = "gray";
+        document.getElementById("pos" + parkingspot).style.backgroundColor = "gray";
     },
 
     clearOutput: function() {
@@ -150,5 +157,5 @@ unparkACar = () => {
 }
 
 getInfoOfCar = () => {
-    garage.showCarInSpace();
+    garage.showCarInSpot();
 }
